52e965d494b5557d3f995f15d245a3cb01afd573
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2021-12-23 18:16:08 +0700

    init project

---
+++ mtx-api/src/main/java/com/video/mtx/MtxApiApplication.java
@@ -0,0 +1,13 @@
+package com.video.mtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtxApiApplication.class, args);
+	}
+
+}
